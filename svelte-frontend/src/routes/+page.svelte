ec065c2905f665c044751926c5a279030ee9199d
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-09 14:40:49 +0800

    Update page layout and add email input field

--- svelte-frontend/src/routes/+page.svelte
+++ svelte-frontend/src/routes/+page.svelte
@@ -1,8 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
+
+<script lang="ts">
+  let email:string = '';
+</script>
 
 
+<main>
+<div class="auth-container">
+<h1>Welcome back to Biscord</h1>
+<p>
+  We're excited to see you again!
+</p>
 
 
-<style>
+<input bind:value={email} type="text">
+</div>
   
+</main>
+<style>
+
+
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #76ABAE;
+}
+
+.auth-container {
+  width: 400px;
+  padding: 20px;
+  background-color: var(--bg-secondary);
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+
 </style>
